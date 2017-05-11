9212b07f6283b64c126889f64fa93fd4eb7a8e15
vladislav-kazakov <vkazakov@phys.nsu.ru>
2017-05-11 15:47:26 +0700

    initial commit

---
+++ application/classes/Controller/Welcome.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No Direct Script Access');
+ 
+Class Controller_Welcome extends Controller_Template
+{
+    public $template = 'site';
+ 
+    public function action_index()
+    {
+        $this->template->message = 'hello, world!';
+    }
+}
---
+++ application/classes/Model/petroglyph.php
@@ -0,0 +1,5 @@
+<?
+class Model_Petroglyph extends ORM
+{
+
+}
